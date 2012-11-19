6f52ad9da0e7c1d8805f02af65ce2c41b423766a
ycshao <ycshao0402@gmail.com>
2012-11-18 20:55:18 -0500

    Linear Blending

--- opencv/main.cpp
+++ opencv/main.cpp
@@ -12,26 +12,54 @@
 #include <highgui.h>
 #include <iostream>
 
-int main(int argc, char** argv)
+int main( int argc, char** argv )
 {
-	if (argc < 2)
+	if (argc < 3)
 	{
-		std::cout << "Please specify an image." << std::endl;
+		std::cout << "Please specify two images." << std::endl;
 		return -1;
 	}
-	cv::Mat image;
-	image = cv::imread(argv[1], 1);
-	if (!image.data)
-	{
-		std::cout << "Image is not loaded." << std::endl;;
-		return -1;
-	}
-	cv::namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
-	cv::imshow("Display Image", image);
+	double alpha = 0.5; double beta; double input;
+	cv::Mat src1, src2, dst;
 
+	/// Ask the user enter alpha
+	std::cout << " Simple Linear Blender " << std::endl;
+	std::cout << "-----------------------" << std::endl;
+	std::cout << "* Enter alpha [0-1]: ";
+	std::cin >> input;
+
+	/// We use the alpha provided by the user iff it is between 0 and 1
+	if( alpha >= 0 && alpha <= 1 )
+	{
+		alpha = input;
+	}
+
+	/// Read image ( same size, same type )
+	src1 = cv::imread(argv[1]);
+	src2 = cv::imread(argv[2]);
+	if(!src1.data)
+	{
+		std::cout << "Error loading src1." << std::endl;
+		return -1;
+	}
+	if(!src2.data)
+	{
+		std::cout << "Error loading src2." << std::endl;
+		return -1;
+	}
+
+	if (src1.size() != src2.size())
+	{
+		std::cout << "Size of two images are different, please provide two images with the same size." << std::endl;
+		return -1;
+	}
+
+	/// Create Windows
+	cv::namedWindow("Linear Blend", 1);
+	beta = ( 1.0 - alpha );
+	cv::addWeighted( src1, alpha, src2, beta, 0.0, dst);
+	cv::imshow( "Linear Blend", dst );
 	cv::waitKey(0);
-
 	return 0;
 }
-
 
